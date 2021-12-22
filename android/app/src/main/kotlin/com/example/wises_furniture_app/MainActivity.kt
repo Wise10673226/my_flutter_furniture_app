422319889b6bf8d9b597419ca570577f1f300d37
Wise10673226 <wiserichiestone@gmail.com>
2021-12-22 09:17:07 +0000

    wises_furniture_app

---
+++ android/app/src/main/kotlin/com/example/wises_furniture_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wises_furniture_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:wises_furniture_app/screens/HomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
